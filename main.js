ad2f1a752e7350a71bd263988728d50d090c0ffb
Jacky Lu <jackylu@berkeley.edu>
2017-09-16 00:01:42 -0700

    push updates for collaboration

--- main.js
+++ main.js
@@ -2,10 +2,16 @@ $(document).ready(function(){
   var income;
 
   $("#income").submit(function(event){
+    income = $("input").val();
+    income = parseInt(income)
+
+    // if typeOf(income) == 'number':
+    //   alert("invalid income, try again!");
+    //   return
+
     $("#first").addClass("hide");
     $("#rest").removeClass("hide");
     event.preventDefault();
-    income = $("input").val();
     console.log(income);
   });
 }
